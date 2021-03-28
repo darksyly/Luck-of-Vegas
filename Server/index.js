22cb13b2d61a2a312c5ebb2eca5431733c631904
silas keim <silaskeim02@gmail.com>
2021-03-28 15:49:14 +0200

    server + login + datenbankverbindung (noch nicht fertig!!)

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,71 @@
-const express = require('express');
-const app=express();
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-const ip="192.168.178.38";
-const port=34567;
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'vegasdata'
+});
+
+const app = express();
+
+app.use(session({
+	secret: 'supermegasecret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res)=>{
-    res.sendFile("Client/Startseite/index.html", {root:"../"})
+    res.sendFile("Client/Login/login.html", {root:"../"})
 })
+
+app.get('/register', (req, res)=>{
+    res.sendFile("Client/Login/login.html", {root:"../"})
+})
+
 app.get('/style.css', (req, res)=>{
+    res.sendFile("Client/Login/style.css", {root:"../"})
+})
+
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+        response.send('Welcome back, ' + request.session.username + '!');
+        //response.sendFile("Client/Startseite/index.html", {root:"../"})
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+/*
+app.get('/home/style.css', (req, res)=>{
     res.sendFile("Client/Startseite/style.css", {root:"../"})
 })
 app.get('/script.js', (req, res)=>{
@@ -21,5 +79,5 @@ app.get('/Logo.png', (req, res)=>{
 app.get('/login', (req, res)=>{
     res.sendFile("Client/Startseite/index.html", {root:"../"})
 })
-
-app.listen(34567, () => console.log("sepp"));
+*/
+app.listen(34567, () => console.log("working..."));
